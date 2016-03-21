3834e48eea518706d89dd00f138e3d3c50b97d3c
KiAnXineki <galerajimenez@gmail.com>
2016-03-21 19:23:16 +0000

    add comments in settings_example

--- settings_example.py
+++ settings_example.py
@@ -3,5 +3,10 @@ port = 5672
 imports = ['examples.test1',
            'examples.test2']
 
-queues = [{'name': 'rocket1', 'concurrency': 7},
+# support params
+# name (mandatory), string
+# concurrency (mandatory), int
+# durable (optional), boolean
+
+queues = [{'name': 'rocket1', 'durable': True, 'concurrency': 7},
           {'name': 'rocket2', 'concurrency': 1}]
