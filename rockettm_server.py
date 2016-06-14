557f659bbda2df71557a4bc3b01affc05174d156
Alberto Galera <galerajimenez@gmail.com>
2016-06-14 10:50:35 +0200

    hotfix raise

--- rockettm_server.py
+++ rockettm_server.py
@@ -17,7 +17,7 @@ class TimeoutException(Exception):
 @contextmanager
 def time_limit(seconds):
     def signal_handler(signum, frame):
-        raise (TimeoutException, "Timed out!")
+        raise TimeoutException("Timed out!")
     signal.signal(signal.SIGALRM, signal_handler)
     signal.alarm(seconds)
     try:
