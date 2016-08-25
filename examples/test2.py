e9fc958ac168fb3c5f9b874e9d0dda26fbdbb150
Alberto Galera <galerajimenez@gmail.com>
2016-08-25 02:51:49 +0200

    py2 to py3

--- examples/test2.py
+++ examples/test2.py
@@ -3,6 +3,6 @@ from rockettm import task
 
 @task('function2')
 def function2(*args, **kwargs):
-    print "args", args
-    print "kwargs", kwargs
+    print("args", args)
+    print("kwargs", kwargs)
     return True
--- examples/test3.py
+++ examples/test3.py
@@ -3,7 +3,7 @@ from rockettm import task
 
 @task('function3', 5)
 def function2(*args, **kwargs):
-    print "args", args
-    print "kwargs", kwargs
+    print("args", args)
+    print("kwargs", kwargs)
     return True
 
